4614b90571cdb35c29a65d678bf80b36eb8a6a3f
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-06-11 16:56:56 -0600

    first way to balance the expression

---
+++ common-ds/js/is-balanced-ex3.js
@@ -0,0 +1,38 @@
+/**
+Exercise 3: Balanced Parentheses
+Problem Statement:
+
+Create a function that checks if a given string of parentheses is balanced. 
+The string can contain the characters (, ), {, }, [ and ]. A string is considered balanced if: 
+
+console.log(isBalanced("()"));           // Output: true
+console.log(isBalanced("()[]{}"));       // Output: true
+console.log(isBalanced("(]"));           // Output: false
+console.log(isBalanced("([)]"));         // Output: false
+console.log(isBalanced("{[]}"));         // Output: true
+ */
+
+const isBalanced = (expression) => {
+
+    let stack = [];
+    const splitPath = expression.split("");
+
+    splitPath.filter((sing, idx) => {
+        if(sing == "(" || sing == "[" || sing == "{" ) {
+            stack.push(sing);
+        }
+
+        if(sing == ")" || sing == "]" || sing == "}" ) { 
+            if(stack.includes("(") && sing == ")") {
+                stack.pop();
+            }     
+        }
+    });
+
+    return  (stack.length == 0) ? true : false;
+}
+
+console.log(isBalanced("()"));
+console.log(isBalanced("()[]{}"));
+console.log(isBalanced("([)]"));
+console.log(isBalanced("{[]}")); 
