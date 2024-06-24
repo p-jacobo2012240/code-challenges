4afce9324b53523881c19872e0887c95c606f6c8
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-06-23 21:39:01 -0600

    first approach for date comparations

---
+++ common-ds/js/date-difference-ex5.js
@@ -0,0 +1,12 @@
+/**
+Exercise 4: Evaluate Reverse Polish Notation (RPN)
+Problem Statement: 
+
+Example:
+dayDifference('Dec 1, 2023', 'Dec 24, 2023');  // Output: 23 
+
+ */
+
+const dayDifference = (date1, date2) => {
+    // Your code here
+};
