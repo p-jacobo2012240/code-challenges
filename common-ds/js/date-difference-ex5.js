c41a0213c340141171dc0f2309e7219e243ad2bd
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-06-25 17:40:06 -0600

    solve difference

--- common-ds/js/date-difference-ex5.js
+++ common-ds/js/date-difference-ex5.js
@@ -9,4 +9,12 @@ dayDifference('Dec 1, 2023', 'Dec 24, 2023');  // Output: 23
 
 const dayDifference = (date1, date2) => {
     // Your code here
+    date1 = ( date1 instanceof Date ) ? date1 : new Date(date1);
+    date2 = (date2 instanceof Date) ? date2 : new Date(date2);
+
+    const difference = Math.abs(date1.getTime() - date2.getTime());
+
+    return (difference / (1000 * 60 * 60 * 24))
 };
+
+console.log(dayDifference('Dec 1, 2023', 'Dec 24, 2023'));
