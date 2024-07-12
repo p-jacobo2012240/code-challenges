56ef2c8dd4a6276eb6c151e76b06c0e6c2bc9ee8
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-07-11 21:59:18 -0600

    prepare statements

---
+++ common-ds/js/leap-year-ex8.js
@@ -0,0 +1,20 @@
+/**
+ * 
+Statement Exercise 8:
+Given a current year, create a program that shows me 
+the leap years that will occur in the next 30 years
+ 
+Example:
+leapYears(2023);
+ 
+//Exit:
+2024
+2028
+2032
+2036
+2040
+2044
+2048
+2052
+ 
+*/
