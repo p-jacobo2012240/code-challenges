9c063a3f1ae8872b0bfa4d3070eecff440ad5277
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-07-11 21:27:19 -0600

    preparestatements

---
+++ common-ds/js/order-by-lastnames.ex7.js
@@ -0,0 +1,19 @@
+/*
+**
+Statement Exercise 7:
+Given a set of people with first and last names
+create a function to sort the array by last name
+in alphabetical order
+ 
+Example:
+sortByLastName([
+    "Jacobo Pablo",
+    "Antonio Albani",
+    "Al Pacino",
+    "monet claude",
+    "Robert de Niro",
+    "George RR Martin",
+    "Sylvester Stallone"
+]);
+
+*/
