0ba41a6a7084215e153364764dd446bbf0390ac6
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-07-17 20:29:38 -0600

    first approach for re ordenation

---
+++ common-ds/js/permutation-ex8.js
@@ -0,0 +1,40 @@
+/**
+Statement Exercise 8:
+Given an array of integers, detect whether that list
+of numbers is a permutation from 1 to the last number in the array.
+ 
+In this case a permutation is a sequence of numbers
+in order without any missing among them.
+ 
+Return the largest missing number.
+ 
+The set may come unordered.
+ 
+Examples:
+permutation([1, 2, 3, 4, 5]) // Returns: 5
+permutation([1, 2, 3, 5])) // Returns: 4
+
+
+*/
+
+
+const permutation = (list) => {
+    
+    let checkValidator = 0;
+    const orderList = list.sort((a, b) => a - b );
+
+    for(let i = 0; i < orderList.length; i++) {
+        if(orderList[i] > (orderList[i] + 1)) {
+            checkValidator = i;
+        } else {
+            checkValidator = orderList[i];
+        }
+    }
+
+    return checkValidator;
+}
+
+
+// TEST CASE
+console.log(permutation([1, 2, 3, 4, 5]));
+console.log(permutation([1, 2, 3, 5]));
