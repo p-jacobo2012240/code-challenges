128cba5b8609ff20e06a84383cd10e21843d9f21
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-07-23 21:28:13 -0600

    adding payload to definition

---
+++ common-ds/js/roman-number-ex9.js
@@ -0,0 +1,18 @@
+/**
+Statement Exercise 8:
+Create an algorithm to which you pass an integer
+and I converted it to Roman numeral
+ 
+Examples:
+integerARoman(100)); // Result: C
+integerARoman(345)); // Result: CCCXLV
+ 
+*/
+
+const integerARoman = (number) => {
+
+
+}
+
+//test case
+integerARoman(345));
