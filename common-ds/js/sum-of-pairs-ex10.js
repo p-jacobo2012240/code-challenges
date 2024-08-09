15133aae48484e7b5928fd5cf954f70c258a700a
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-08-08 21:08:39 -0600

    all test cases are working

--- common-ds/js/sum-of-pairs-ex10.js
+++ common-ds/js/sum-of-pairs-ex10.js
@@ -9,8 +9,18 @@ sumPair([2, 13, 6, 15]); // Result: 8
 
 const sumPair = (nums) => {
 
+    let total = 0;
+
+    for(const num of nums) {
+        if(num % 2 == 0 ) {
+            total += num;
+        }
+    }
+
+    return total;
 };
 
 //test case
-sumPair([2, 13, 6, 15]);
+console.log(sumPair([2, 13, 6, 15]));
+console.log(sumPair([101, 12, 6, 3, 25]));
 
