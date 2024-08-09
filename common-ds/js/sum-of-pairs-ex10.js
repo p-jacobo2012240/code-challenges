76afbde53667ba00b85e2b7ccc8cfbe56d4b86e8
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-08-08 21:01:50 -0600

    declare statements

---
+++ common-ds/js/sum-of-pairs-ex10.js
@@ -0,0 +1,16 @@
+/**
+Statement Exercise 10:
+Create a function that receives an array of numbers and calculates
+the sum of all the even numbers in the array
+
+Examples:
+sumPair([2, 13, 6, 15]); // Result: 8 
+ */
+
+const sumPair = (nums) => {
+
+};
+
+//test case
+sumPair([2, 13, 6, 15]);
+
