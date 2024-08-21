f26c0438e7ffaaab023867dab24bb9a0bffab6ac
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-08-20 20:55:28 -0600

    prepare a signature

---
+++ common-ds/js/resources/sumTwo-ex12.js
@@ -0,0 +1,17 @@
+/**
+*
+Statement Exercise 11: Create a function to which you pass an array of numbers and a number 
+that will be the result of the sum of two of the values. 
+
+Examples: 
+    sumTwo([3, 7, 8, 2], 10) // [ 3, 7] (both add up to 10) 
+    sumTwo([4, 5, 9, 1], 10) // [9, 1] 
+    sumTwo([1, 2, 3, 4], 5) // [2, 3 ]  
+ */
+
+const sumTwo = (list, result) => {
+    // build a logic.
+}
+
+// test case
+console.log(sumTwo([4, 5, 9, 1], 10)) // [9, 1] 
