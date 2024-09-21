9e8313b1502f3179160d9ae32f9f3e03d9a6ea76
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-09-20 21:28:03 -0600

    making the sign

---
+++ common-ds/js/count-words-ex13.js
@@ -0,0 +1,33 @@
+/**
+ * 
+Statement Exercise 12:
+Create a program that counts the words in a text
+
+Examples:
+countWords("Saint Roque's dog does not have a tail
+because he is a very very bad dog");
+
+Returns:
+{
+the: 2,
+dog: 2,
+of: 1,
+saint: 1,
+roque: 1,
+not: 1,
+has: 1,
+tail: 1,
+because: 1,
+is: 1,
+a: 1,
+very: 2,
+bad: 1
+}
+
+*/ 
+
+const countWords = (text) => {
+    // do the magic..
+}
+
+console.log(countWords("Saint Roque's dog does not have a tail because he is a very very bad dog"));
