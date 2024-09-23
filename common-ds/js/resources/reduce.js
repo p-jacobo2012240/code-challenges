3f5cc828713afae3058245e1ec4d6254a413ceb3
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-09-23 16:50:33 -0600

    example of reduce was added

--- common-ds/js/resources/reduce.js
+++ common-ds/js/resources/reduce.js
@@ -5,6 +5,24 @@ const people = [
     { name: 'Pablo J', age: 25 }
 ]
 
+const cart = [
+    { name: 'Laptop', quantity: 1, price: 999 },
+    { name: 'Mouse', quantity: 2, price: 20 },
+    { name: 'Keyboard', quantity: 1, price: 50 },
+    { name: 'Monitor', quantity: 2, price: 200 }
+];
+
+const colors = ['red', 'blue', 'green', 'blue', 'yellow', 'blue', 'red'];
+
+
+// the sum of ages
+const sum = people.reduce((accumulator, current) => { 
+    return accumulator + current.age;
+}, 0);
+
+console.log('sum of ages ', sum);
+
+
 // return the most younger
 const result = people.reduce((prev, current) => {
     if(prev.age < current) {
@@ -33,3 +51,20 @@ const output = people.reduce((prev, current, idx, array) => {
 
 // initials
 console.log('initials ', output);
+
+
+// total cost of items in the cart.
+const totalItems = cart.reduce((prev, current) => {
+    return prev + ( current.quantity * current.price);
+}, 0);
+
+
+console.log('total cart ', totalItems);
+
+
+// counting specific color item
+const findABlue = colors.reduce((prev, value) => {
+    return (value == 'blue') ? prev + 1 : prev;
+},0)
+
+console.log('blue ' , findABlue );
