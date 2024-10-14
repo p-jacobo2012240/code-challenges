17bed5077f52344b86be6c48003b09257873111a
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-10-14 15:47:25 -0600

    resources by the challenge

---
+++ common-ds/js/resources/dummy-ex29.js
@@ -0,0 +1,15 @@
+
+const movies = [
+    { title: "Terminator", genre: "action", ReleaseYear: 1984 },
+    { title: "Alien", genre: "science fiction", ReleaseYear: 1979 },
+    { title: "Die Hard", genre: "action", ReleaseYear: 1988 },
+    { title: "Predator", genre: "action", ReleaseYear: 1987 },
+    { title: "Total Recall", genre: "science fiction", ReleaseYear: 1990 },
+    { title: "RoboCop", genre: "sci-fi", yearRelease: 1987 },
+    { title: "Starship Troopers", genre: "sci-fi", yearRelease: 1997 },
+    { title: "The Fifth Element", genre: "sci-fi", yearRelease: 1997 },
+    { title: "Armageddon", genre: "action", yearRelease: 1998 },
+    { title: "Deep Impact", genre: "sci-fi", yearRelease: 1998 }
+]; 
+    
+module.exports = movies;
