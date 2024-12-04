f0cac2f5d67f22e1755edd619a3976dfe7a4351b
p-jacobo2012240 <pablojacobo1998@gmail.com>
2024-12-03 21:21:05 -0600

    feat: findPairs outcome config

---
+++ common-ds/js/medium/find-ext-number.ex36.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * Statement Exercise 36:
+ * Create a program that finds the pairs of numbers that result
+ * in a number that we pass to the function as a parameter.
+ * 
+ * 
+ * Examples:
+ * findPairsWithSum([1, 2, 3, 4], 5);
+ * 
+ * Returns:
+ * [ [ 1, 4 ], [ 2, 3 ] ]
+ * 
+ */
+
+const findPairsWithSum = (list, result ) => {
+    // WIP   
+}
+
+// test case
+console.log(findPairsWithSum([4, 5, 6, 7], 8));
